50501911fe20bc910c2eb9084781b77586ca1875
sikleo <937330770@qq.com>
2022-02-04 21:07:19 +0800

    列表搜索字段完成

--- src/pages/User/index.jsx
+++ src/pages/User/index.jsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-02-04 06:30:56
- * @LastEditTime: 2022-02-04 20:18:42
+ * @LastEditTime: 2022-02-04 21:03:43
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \umi_shop\src\pages\User\index.jsx
@@ -55,6 +55,7 @@ const User = () => {
         },
         {
             title:'操作',
+            hideInSearch:true,
             render:(_,record)=><a onChange={()=>{}}>编辑</a>
                
             
@@ -65,24 +66,13 @@ const User = () => {
         <ProTable
       columns={columns}
       actionRef={actionRef}
-      request={async () =>  getUsers() }
+      request={async (params = {}, sort, filter) =>  getUsers(params) }
       
       rowKey="id"
       search={{
         labelWidth: 'auto',
       }}
-      form={{
-        // 由于配置了 transform，提交的参与与定义的不同这里需要转化一下
-        syncToUrl: (values, type) => {
-          if (type === 'get') {
-            return {
-              ...values,
-              created_at: [values.startTime, values.endTime],
-            };
-          }
-          return values;
-        },
-      }}
+    
       pagination={{
         pageSize: 8,
       }}
--- src/services/user.js
+++ src/services/user.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-01-27 22:17:33
- * @LastEditTime: 2022-02-04 20:11:29
+ * @LastEditTime: 2022-02-04 21:04:35
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \umi_shop\src\services\user.js
@@ -14,6 +14,6 @@ export async function queryCurrent() {
 }
 
 //获取用户列表
-export async function getUsers() {
-  return request('/admin/users');
+export async function getUsers(params) {
+  return request('/admin/users',{params});
 }
