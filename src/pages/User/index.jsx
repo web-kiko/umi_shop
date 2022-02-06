8c51904ff9db648b3094e862f2ff2ed08ced1aae
sikleo <937330770@qq.com>
2022-02-06 16:23:37 +0800

    用户列表得添加新用户完成

--- src/pages/User/index.jsx
+++ src/pages/User/index.jsx
@@ -1,19 +1,22 @@
 /*
  * @Author: your name
  * @Date: 2022-02-04 06:30:56
- * @LastEditTime: 2022-02-04 22:22:45
+ * @LastEditTime: 2022-02-06 16:19:49
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \umi_shop\src\pages\User\index.jsx
  */
 import { PageContainer } from '@ant-design/pro-layout'
-import React,{useRef} from 'react'
-import { Button,Avatar,Switch, message } from 'antd';
+import React,{useRef,useState} from 'react'
+import { Button,Avatar,Switch, message,Modal } from 'antd';
 import ProTable from '@ant-design/pro-table';
 import { PlusOutlined,UserOutlined} from '@ant-design/icons';
-import { getUsers,patchLock } from '@/services/user';
+import { getUsers,patchLock,addUser } from '@/services/user';
+import ProForm, { ProFormText} from '@ant-design/pro-form';
 const User = () => {
+    //ref刷新表格
     const actionRef = useRef();
+    const [isModalVisible,setIsModalVisible] =useState(false);
     const columns = [
         {
             title:'头像',
@@ -67,7 +70,36 @@ const User = () => {
                
             
         },
-    ]   
+    ] 
+    /**
+     * 打开模态框
+     *  */ 
+     const openModel=()=>{
+        setIsModalVisible(true)
+    }
+    
+    /**
+     * 关闭模态框
+     * 
+     */
+     
+    const colseModel=()=>{
+        setIsModalVisible(false)
+    }
+    /**
+     * 发送请求添加用户
+     */
+    const addUsers= async(values)=>{
+        const respones =await addUser(values)
+        if(respones.status===undefined){
+            message.success('添加成功');
+            //刷新
+            actionRef.current.reload();
+           
+            setIsModalVisible(false);
+           
+        }
+    }
      return (
         <PageContainer>
         <ProTable
@@ -81,18 +113,55 @@ const User = () => {
       }}
     
       pagination={{
-        pageSize: 8,
+        pageSize: 10,
       }}
       dateFormatter="string"
       headerTitle="用户列表"
       toolBarRender={() => [
-        <Button key="button" icon={<PlusOutlined />} type="primary">
+        <Button key="button" icon={<PlusOutlined />} type="primary" onClick={()=> openModel()}>
           新建
         </Button>,
        
       ]}
     />
   );
+  <Modal title="添加用户" 
+         visible={isModalVisible}  
+         onCancel={colseModel}
+         destroyOnClose={true}
+          footer={null}>
+         <ProForm
+              onFinish={ (values) => {
+                addUsers(values)
+                message.success('提交成功');
+              }}
+            >
+             <ProFormText
+                  name="name"
+                  label="昵称"
+                  tooltip="最长为 24 位"
+                  placeholder="请输入昵称"
+                  rules={[{required:true,message:'请输入昵称'}]}
+              />
+               <ProFormText
+                  name="email"
+                  label="邮箱"
+                  tooltip="最长为 24 位"
+                  placeholder="请输入邮箱"
+                  rules={[{required:true,message:'请输入邮箱'},
+                  {type:'email',message:"邮箱格式不正确"}]}
+              />
+               <ProFormText.Password
+                  name="password"
+                  label="密码"
+                  tooltip="最长为 24 位"
+                  placeholder="请输入密码"
+                  rules={[{required:true,message:'请输入密码'},
+                  {min:6,message:"密码不少于6位"}
+                  ]}
+              />
+        </ ProForm>
+      </Modal>
         </PageContainer>
     )
 }
--- src/services/user.js
+++ src/services/user.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-01-27 22:17:33
- * @LastEditTime: 2022-02-04 21:18:59
+ * @LastEditTime: 2022-02-06 15:43:36
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \umi_shop\src\services\user.js
@@ -20,4 +20,8 @@ export async function getUsers(params) {
 //获取用户列表的禁用和启用
 export async function patchLock(uid) {
   return request.patch(`/admin/users/${uid}/lock`);
+}
+//添加用户
+export async function addUser(params) {
+  return request.post('/admin/users',{params});
 }
